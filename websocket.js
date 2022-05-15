f96e34e015b3cb8f7b5a535acf292488ba081382
Jefferson Paixao <jeffersonhsp@live.com>
2022-05-15 01:57:41 -0300

    Configure port 2

--- websocket.js
+++ websocket.js
@@ -1,4 +1,4 @@
-var WebSocketServer = require('websocket').server;
+var WebSocketServer = require('ws').server;
 var http = require('http');
 
 //Porta que o server irá escutar  
@@ -16,40 +16,20 @@ server.listen(port, () => {
     console.log(`Server está executando na porta ${port}`);
 });
 
-//Cria o WebSocket server
-wsServer = new WebSocketServer({
-  httpServer: server
-});
 
 //Chamado quando um client deseja conectar
-wsServer.on('request', (request) => {
-    //Estado do led: false para desligado e true para ligado
-    let state = false;
+var wss = new WebSocketServer({server: server})
+console.log("websocket server created")
 
-    //Aceita a conexão do client
-    let client = request.accept(null, request.origin);
+wss.on("connection", function(ws) {
+  var id = setInterval(function() {
+    ws.send(JSON.stringify(new Date()), function() {  })
+  }, 1000)
 
-    //Chamado quando o client envia uma mensagem
-    client.on('message', (message) => {
-        //Se é uma mensagem string utf8
-        if (message.type === 'utf8') {
-            //Mostra no console a mensagem
-            console.log(message.utf8Data);
-        }
-    });
+  console.log("websocket connection open")
 
-    //Cria uma função que será executada a cada 1 segundo (1000 millis) para enviar o estado do led
-    let interval = setInterval(() => {
-        //Envia para o client "ON" ou "OFF" dependendo do estado atual da variável state
-        client.sendUTF(state? "ON" : "OFF");
-        //Inverte o estado
-        state = !state;
-    }, 3000);//Tempo entre chamadas => 1000 millis = 1 segundo 
-
-    //Chamado quando a conexão com o client é fechada
-    client.on('close', () => {
-        console.log("Conexão fechada");
-        //Remove o intervalo de envio de estado
-        clearInterval(interval);
-    });
-});
+  ws.on("close", function() {
+    console.log("websocket connection close")
+    clearInterval(id)
+  })
+})
