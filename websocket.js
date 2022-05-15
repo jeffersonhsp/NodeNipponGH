53f2b180e77aee0a6f7822c08e0cb19b31ebcafa
Jefferson Paixao <jeffersonhsp@live.com>
2022-05-15 12:49:00 -0300

    Configure port 2

--- websocket.js
+++ websocket.js
@@ -43,7 +43,7 @@ wsServer.on('request', (request) => {
         client.sendUTF(state? "ON" : "OFF");
         //Inverte o estado
         state = !state;
-    }, 500);//Tempo entre chamadas => 1000 millis = 1 segundo 
+    }, 300);//Tempo entre chamadas => 1000 millis = 1 segundo 
 
     //Chamado quando a conexão com o client é fechada
     client.on('close', () => {
