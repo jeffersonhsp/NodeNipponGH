df99bd4cc16f9f159fb7800b4280645b77ea7bb4
Jefferson Paixao <jeffersonhsp@live.com>
2022-05-15 02:01:31 -0300

    Configure port 2

--- websocket.js
+++ websocket.js
@@ -6,8 +6,7 @@ const port = process.env.PORT || 8080;
 
 //Cria o server
 var server = http.createServer(function(req,res){
-    //res.end('<h1> Ok </h1> ${port}')
-    res.end(`Server está executando na porta ${port}`);
+    res.end("<h1> Ok </h1>")
 });
 
 //Server irá escutar na porta definida em 'port'
@@ -16,20 +15,40 @@ server.listen(port, () => {
     console.log(`Server está executando na porta ${port}`);
 });
 
+//Cria o WebSocket server
+wsServer = new WebSocketServer({
+  httpServer: server
+});
 
 //Chamado quando um client deseja conectar
-var wss = new WebSocketServer({server: server})
-console.log("websocket server created")
+wsServer.on('request', (request) => {
+    //Estado do led: false para desligado e true para ligado
+    let state = false;
 
-wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+    //Aceita a conexão do client
+    let client = request.accept(null, request.origin);
 
-  console.log("websocket connection open")
+    //Chamado quando o client envia uma mensagem
+    client.on('message', (message) => {
+        //Se é uma mensagem string utf8
+        if (message.type === 'utf8') {
+            //Mostra no console a mensagem
+            console.log(message.utf8Data);
+        }
+    });
 
-  ws.on("close", function() {
-    console.log("websocket connection close")
-    clearInterval(id)
-  })
-})
+    //Cria uma função que será executada a cada 1 segundo (1000 millis) para enviar o estado do led
+    let interval = setInterval(() => {
+        //Envia para o client "ON" ou "OFF" dependendo do estado atual da variável state
+        client.sendUTF(state? "ON" : "OFF");
+        //Inverte o estado
+        state = !state;
+    }, 3000);//Tempo entre chamadas => 1000 millis = 1 segundo 
+
+    //Chamado quando a conexão com o client é fechada
+    client.on('close', () => {
+        console.log("Conexão fechada");
+        //Remove o intervalo de envio de estado
+        clearInterval(interval);
+    });
+});
